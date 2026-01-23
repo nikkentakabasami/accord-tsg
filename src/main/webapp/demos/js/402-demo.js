6ef6e89c53d78503552a0eec5f3c41d250b758e0
tetsuma <tetsuma@devel-tester.alpha>
2026-01-23 18:43:27 +0300

    trigger

--- src/main/webapp/demos/js/402-demo.js
+++ src/main/webapp/demos/js/402-demo.js
@@ -3,11 +3,12 @@
 
 let $workPanel;
 
-let $btn1, $btn2, $inp1, $inp2, $inp3, $inp4;
+let $btn1, $btn2, $inp1, $inp2, $inp3, $inp4, $btnTests;
+
+
 
 
 function clickHandler(event) {
-  //	log(event.pageX + ", " + event.pageY);
   logNL();
   log(`event (type=${event.type})`);
   logObject(event, 'currentTarget', "pageX", "pageY");
@@ -15,6 +16,9 @@ function clickHandler(event) {
 }
 
 function simpleHandler(event) {
+//	event.preventDefault();
+//	event.stopPropagation();	
+	
 	log(`event (type=${event.type})`);
 }
 
@@ -25,38 +29,58 @@ let selectorsData1 = {
 	//назначает обработчик
 	return $(".panel1 input").bind("click", clickHandler);
   },
-  off_inp1: function() {
-	//убирает все обработчики событий, привязанные к этому элементу
-	return $inp1.off();
+  
+  on_inp1_click: function() {
+  //назначает обработчик
+  return $inp1.on("click", clickHandler);
   },
-  unbind_inp1: function() {
-	//убирает обработчик
-	return $inp2.unbind("click", clickHandler);
-  },
-
-  bind_two_events: function() {
+  
+  on_two_events: function() {
 	//назначает обработчик на 2 события
-	return $inp1.bind("mouseenter mouseleave", event => {
+	return $inp1.on("mouseenter mouseleave", event => {
 	  $inp1.toggleClass("green-border");
 	});
   },
 
-  bind_inp3_click: function() {
-	//с передачей data-объекта
-	return $inp3.bind("click", { msg: "Spoon!" }, event => {
+  on_data_param: function() {
+	//с передачей data-объекта, который можно получить из event
+	$inp3.on("click", { msg: "Spoon!" }, event => {
 	  log(`event (type=${event.type})`);
 	  log(event.data.msg);
 	});
+	
+	$inp2.on("click", event => {
+		//с передачей data-объекта в trigger
+		$inp3.trigger( "click", { msg: "Trigger message!" } );
+	});
+	
+	
+  },
+
+  trigger_data: function() {
+	
+	$btnTests.text("trigger click on inp3").on("click", event => {
+		//вызов события с передачей data-объекта
+		$inp3.trigger( "click", { msg: "Trigger message!" } );
+	});	
+	
+	
+	//Получение data-объекта, переданного в trigger
+	$inp3.on("click", (event, data) => {
+	  log("inp3 click");
+	  if (data) {
+		log(data.msg);
+	  }
+	});
+
+	
+	
+//	$inp3.trigger( "click", { msg: "Spoon!" } );
   },
 
 
-  on_inp1_click: function() {
-	//назначает обработчик
-	return $inp1.on("click", clickHandler);
-  },
-
-  on_inp_click: function() {
-	//назначает обработчик
+  on_selector: function() {
+	//с передачей дополнительного селектора
 	return $(".panel1").on("click", " input:text", clickHandler);
   },
 
@@ -70,24 +94,25 @@ let selectorsData1 = {
 
   },
 
+  
+  off_inp1: function() {
+	//убирает все обработчики событий, привязанные к этому элементу
+	return $inp1.off();
+  },
+  unbind_inp1: function() {
+	//убирает обработчик
+	return $inp2.unbind("click", clickHandler);
+  },
+  
 
 
 
 
-  /*	
-  $( "#foo" ).bind({
-	click: function() {
-	  // Do something on click
-	},
-	mouseenter: function() {
-	  // Do something on mouseenter
-	}
-  });	
-  $( "#foo" ).bind( "click", 
-	  { msg: "Spoon!"}, 
-	  function( event ) {
-		alert( event.data.msg );
+  /*
+  $( "div" ).on( "click", function( event, person ) {
+    alert( "Hello, " + person.name );
   });
+  $( "div" ).trigger( "click", { name: "Jim" } );
 */
 
 
@@ -116,6 +141,8 @@ function reloadSandbox() {
   $inp2 = $("#inp2");
   $inp3 = $("#inp3");
   $inp4 = $("#inp4");
+
+  $btnTests = $("#btnTests"); 
 
 
 
